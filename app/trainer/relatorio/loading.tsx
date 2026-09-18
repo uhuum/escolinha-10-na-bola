4fc38239a14558fd7f1b53f6ec1aad26812a8c34
uhuum <arthur950370@gmail.com>
2026-09-18 08:53:58 -0300

    Adiciona splash temático aos registros dos treinadores

---
+++ app/trainer/relatorio/loading.tsx
@@ -0,0 +1,5 @@
+import { SectionLoadingScreen } from "@/components/section-loading-screen"
+
+export default function TrainerRecordsLoading() {
+  return <SectionLoadingScreen title="Carregando registros dos treinadores..." description="Organizando chamadas, presenças e relatórios dos treinos..." />
+}
